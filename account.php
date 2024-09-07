dd2abf7d80ac6295c2bff8402a429457ed74ecef
may-thuh <159943780+may-thuh@users.noreply.github.com>
2024-09-08 03:58:47 +0630

    Update account.php

--- account.php
+++ account.php
@@ -39,8 +39,8 @@ $conn->close();
         <nav class="navbar">
             <ul>
                 <li><a href="home.php">Home</a></li>
-                <li><a href="#videos-section">Videos</a></li>
-                <li><a href="#recipes-section">Recipes</a></li>
+                <li><a href="home.php#videos-section">Videos</a></li>
+                <li><a href="#home.php#recipes-section">Recipes</a></li>
                 <?php if (isset($_SESSION['user_id'])) : ?>
                 <li><a href="account.php"><i class="fa fa-user"></i> Account</a></li>
                 <li><a href="logout.php">Log Out</a></li>
@@ -53,12 +53,24 @@ $conn->close();
         <main>
             <section class="profile">
                 <div class="pfp">
-                    <img src="images.jpg" alt="Profile Picture">
+                    <img src="img/girl.jpg" alt="Profile Picture">
                 </div>
                 <div class="about-me">
                     <h2>About Me</h2>
                     <p>
-                       Hello, my name is <?php echo htmlspecialchars($username); ?>.
+                       Hello, my name is <?php echo htmlspecialchars($username); ?>, and I’ve been immersed in the world of culinary arts for over ten years.
+                     My journey began at the Culinary Institute of America, where I developed a deep passion for
+                      creating exceptional dishes. Happy Cooking!
+                    </p>
+                    <p>
+                    I’ve worked in diverse environments, from lively bistros to upscale restaurants, specializing in farm-to-table cuisine.
+                     I love using fresh, locally sourced ingredients to craft memorable dishes like my roasted heirloom beet
+                      salad and seared scallops with citrus beurre blanc. <?php echo htmlspecialchars($username); ?>.
+                    </p>
+                    <p>
+                    When I’m not in the kitchen, you’ll find me exploring new food trends, experimenting with recipes,
+                     and traveling to gather fresh culinary inspiration. I’m dedicated to pushing the boundaries of gastronomy
+                      and making a lasting impact in the culinary world. <?php echo htmlspecialchars($username); ?>.
                     </p>
                 </div>
             </section>
@@ -75,19 +87,19 @@ $conn->close();
                 <div class="people">
                     <div class="person">
                         <div class="circle">
-                           <img src="images.jpg" alt="Person 1">
+                           <img src="img/man1.jpg" alt="Person 1">
                         </div>
                         <p>Name 1</p>
                     </div>
                     <div class="person">
                         <div class="circle">
-                            <img src="images.jpg" alt="Person 2">
+                            <img src="img/lady.jpg" alt="Person 2">
                         </div>
                         <p>Name 2</p>
                     </div>
                     <div class="person">
                         <div class="circle">
-                           <img src="images.jpg" alt="Person 3">
+                           <img src="img/man2.jpg" alt="Person 3">
                         </div>
                         <p>Name 3</p>
                     </div>
